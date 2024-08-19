f7207103685179df4a2e1693f1f236f8cdf45340
MemphiS047 <hakverdiyigit@gmail.com>
2024-08-19 16:20:24 +0300

    Kullanici karsilama fonksiyonalitesi eklendi

---
+++ src/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greet")
+    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name + "!";
+    }
+}
+
+
