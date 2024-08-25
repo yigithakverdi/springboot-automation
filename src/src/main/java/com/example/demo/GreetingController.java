670c0a9cce92e1d0ab9e08a548a6f72a3442d672
Yigit Hakverdi <hakverdiyigit@gmail.com>
2024-08-25 20:19:06 +0300

    Jenkins #24'uncu calistirma eklendi

--- src/src/main/java/com/example/demo/GreetingController.java
+++ src/src/main/java/com/example/demo/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
     @GetMapping("/greet")
     public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
         return "Hello " + name + "!";
